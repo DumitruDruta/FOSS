a58e95902a575978962e3a90e99f605823e29adb
Mahmoud <aljouhar@rptu.de>
2023-12-03 00:28:52 +0100

    fixin eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    "standard",
-    "plugin:react/recommended"
-  ],
+  extends: 'standard',
   overrides: [
     {
       env: {
