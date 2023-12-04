c2bb4d6dbe12287a3339b2189fac9590e29b29f4
Mahmoud <aljouhar@rptu.de>
2023-12-04 23:26:39 +0100

    eslint-disable-next-line no-unused-vars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React, { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles.css"
 
+// eslint-disable-next-line no-unused-vars
 import App from "./App"
 
 const root = createRoot(document.getElementById("root"))
